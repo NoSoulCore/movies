53848791afd2c4930398195df9a8c2d32ca25b18
NoSoulCore <nosoulcore@gmail.com>
2023-05-25 00:12:23 +0200

    if statements in Twig. and data exchange of View and Controller

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,14 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'movies')]
-    public function index($name): JsonResponse
+    #[Route('/movie', name: 'movies')]
+    public function index(): Response
     {
-        return $this->render();
+        return $this->render('index.html.twig', [
+            'title' => 'Avengers: Endgame'
+        ]);
     }
 }
