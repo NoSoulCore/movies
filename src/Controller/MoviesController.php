992a53414f8055353f5d51227a2854a89b7af913
NoSoulCore <nosoulcore@gmail.com>
2023-05-30 17:02:49 +0200

    use movie entity with dd method
    ...15 minut to understand to add movie Entity to movie Controller... error: No such class as Movie

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
 use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movie', name: 'movies')]
-    public function index(MovieRepository $movieRepository): Response
+    #[Route('/movies', name: 'movies')]
+    public function index(EntityManagerInterface $em): Response
     {
-        $movies = $movieRepository->findAll();
+        $repository = $em->getRepository(Movie::class);
+        $movies = $repository->findAll();
+
+        dd($movies);
+
         return $this->render('index.html.twig');
     }
 }
