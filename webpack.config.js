ab47cea3ee6f8039e52a159158feae59f2a2b8de
NoSoulCore <nosoulcore@gmail.com>
2023-05-31 13:21:04 +0200

    relations betwen tailwindcss and post css was added. tailwind now can be used in project

--- webpack.config.js
+++ webpack.config.js
@@ -71,6 +71,15 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    .enablePostCssLoader((options) => 
+    {
+        options.postcssOptions = 
+        {
+            config: './postcss.config.js'
+        }
+    }
+    )
 ;
 
 module.exports = Encore.getWebpackConfig();
