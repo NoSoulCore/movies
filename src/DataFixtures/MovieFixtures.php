4226d1b8099a31cd3400cd935540a19890b8fe47
NoSoulCore <nosoulcore@gmail.com>
2023-06-01 23:53:26 +0200

    changed fixtures (static data in db)

--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -14,7 +14,7 @@ class MovieFixtures extends Fixture
         $movie->setTitle('The Dark Knight');
         $movie->setReleaseYear(2009);
         $movie->setDescription("This is the description of the Dark Knight");
-        $movie->setImagePath('https://cdn.pixabay.com/photo/2016/11/18/17/02/cave-1835823_1280.jpg');
+        $movie->setImagePath('https://cdn.pixabay.com/photo/2019/06/25/12/35/templar-4298112_1280.jpg');
 
         //Add data to Pivot Table
         $movie->addActor($this->getReference('actor_1'));
@@ -25,7 +25,7 @@ class MovieFixtures extends Fixture
         $movie2->setTitle('One Punch Man');
         $movie2->setReleaseYear(2017);
         $movie2->setDescription("This is the description of the One Punch Man");
-        $movie2->setImagePath('https://static.wikia.nocookie.net/onepunchman/images/8/82/Saitama_Manga.png/revision/latest?cb=20220825051134');
+        $movie2->setImagePath('https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/One_Punch_Man_logo.svg/1920px-One_Punch_Man_logo.svg.png');
         //Add data to Pivot Table
         $movie->addActor($this->getReference('actor_3'));
         $movie->addActor($this->getReference('actor_4'));
